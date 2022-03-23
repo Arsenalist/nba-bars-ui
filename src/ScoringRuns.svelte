a8dc7dbf6c01a673c4eb57bc2199adb26eb6c8a4
Zarar Siddiqi <zarars@gmail.com>
2022-03-23 13:04:57 -0400

    Componentize

--- src/ScoringRuns.svelte
+++ src/ScoringRuns.svelte
@@ -1,30 +1,14 @@
 <script>
+    import TeamScoringRun from "./TeamScoringRun.svelte";
     export let scoringRunData;
     export let boxScore;
-    function displayRun(run) {
-        return `${parseInt(run.endAction.scoreAway)-parseInt(run.startAction.scoreAway)}-${parseInt(run.endAction.scoreHome)-parseInt(run.startAction.scoreHome)} in ${run.runDurationDisplay}`;
-    }
 </script>
 {#if scoringRunData}
-<table class="table">
-    <thead>
-    <tr>
-        <th>Start</th>
-        <th>End</th>
-        <th>Run</th>
-        <th>Difference</th>
-    </tr>
-    </thead>
-    <tbody>
-{#each scoringRunData.homeScoringRuns as run}
-
-    <tr>
-        <td><span class="badge  bg-light text-dark">{run.startAction.clock}</span> {run.startAction.scoreAway}-{run.startAction.scoreHome}</td>
-        <td>{run.endAction.scoreAway}-{run.endAction.scoreHome} <span class="badge  bg-light text-dark">{run.endAction.clock}</span></td>
-        <td>{displayRun(run)}</td>
-        <td>{run.difference}</td>
-    </tr>
-{/each}
-    </tbody>
-</table>
+    <p>
+        Shows top five scoring runs for each team. The maximum duration of a run is 15 minutes.
+    </p>
+    <h3>{boxScore.awayTeam.teamName}</h3>
+    <TeamScoringRun scoringRuns={scoringRunData.awayScoringRuns}/>
+    <h3>{boxScore.homeTeam.teamName}</h3>
+    <TeamScoringRun scoringRuns={scoringRunData.homeScoringRuns}/>
 {/if}
---
+++ src/TeamScoringRun.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let scoringRuns = []
+    function displayRun(run) {
+        return `${parseInt(run.endAction.scoreAway)-parseInt(run.startAction.scoreAway)}-${parseInt(run.endAction.scoreHome)-parseInt(run.startAction.scoreHome)} in ${run.runDurationDisplay}`;
+    }
+</script>
+<table class="table">
+    <thead>
+    <tr>
+        <th>Start</th>
+        <th>End</th>
+        <th>Run</th>
+        <th>Difference</th>
+    </tr>
+    </thead>
+    <tbody>
+    {#each scoringRuns as run}
+        <tr>
+            <td><span class="badge  bg-light text-dark">{run.startClockDisplay}</span> {run.startAction.scoreAway}-{run.startAction.scoreHome}</td>
+            <td>{run.endAction.scoreAway}-{run.endAction.scoreHome} <span class="badge  bg-light text-dark">{run.endClockDisplay}</span></td>
+            <td>{displayRun(run)}</td>
+            <td>{run.difference}</td>
+        </tr>
+    {/each}
+    </tbody>
+</table>
