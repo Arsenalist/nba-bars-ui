17eedcd289d5f75f954febaf5ac8f0288592bf85
Zarar Siddiqi <zarars@gmail.com>
2022-03-24 11:17:22 -0400

    Improved description of a run

--- src/ScoringRuns.svelte
+++ src/ScoringRuns.svelte
@@ -5,7 +5,7 @@
 </script>
 {#if scoringRunData}
     <p>
-        Shows top five scoring runs for each team. The maximum duration of a run is 15 minutes.
+        Shows top scoring runs for each team. The maximum duration of a run is 15 minutes and only runs which exceed a differential of 8 or more are shown.
     </p>
     <h3>{boxScore.awayTeam.teamName}</h3>
     <TeamScoringRun boxScore={boxScore} scoringRuns={scoringRunData.awayScoringRuns}/>
